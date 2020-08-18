6c0331b3f4eef1d75164e75824a80dcedda98bb4
se7en <14759920@qq.com>
2020-08-18 13:34:24 +0800

    项目设置

--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,18 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    // mock
+    before: require('./mock/mock-server.js'),
+    // 代理转发
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxxx:8888',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       ['^' + process.env.VUE_APP_BASE_API]: ''
+    //     }
+    //   },
+    // },
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
